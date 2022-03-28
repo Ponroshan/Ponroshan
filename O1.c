86496c3c4426941a91d4cb53bc0fd613caf956fc
Ponroshan <102470013+Ponroshan@users.noreply.github.com>
2022-03-28 14:57:02 +0530

    Create O1.c

---
+++ O1.c
@@ -0,0 +1,26 @@
+- 👋 Hi, I’m @Ponroshan
+#include<stdio.h>
+int main()
+{
+int i,j,n;
+scanf("%d",&n);
+for(i=0;i<=n;i++)
+{
+for(j=0;j<=i;j++)
+{
+if(j%2==1)
+{
+printf("1");
+}
+else
+{
+printf("0");
+}
+}
+printf("\n");
+}
+return 0;
+}
+
+
+    
